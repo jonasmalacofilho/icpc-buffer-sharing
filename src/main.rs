jonasmalacofilho/icpc-buffer-sharing

commit ba5ab6f49813e4b537f562f72fbe74882fd7c640

Simplify case IV (Arc & Virt miss)
Inspired-by: https://github.com/al8n/caches-rs/blob/5a9eb4031e64/src/lru/adaptive.rs#L430-L445
Inspired-by: https://github.com/jedisct1/rust-arc-cache/blob/4ee3ba285f60/src/lib.rs#L140-L153

src/main.rs
@@ -190,36 +190,25 @@ impl Buffer {
             // Cache miss in Arc and Virt. Move to MRU position in T1.
             self.counters[t].misses += 1;
 
-            let t1_len: usize = self.arc_t1.iter().map(|x| x.len()).sum::<usize>();
-            let t2_len: usize = self.arc_t2.iter().map(|x| x.len()).sum::<usize>();
-            let b1_len = self.arc_b1.len();
-            let b2_len = self.arc_b2.len();
             let at_qmax =
                 (self.arc_t1[t].len() + self.arc_t2[t].len()) == self.params.buffer_sizes_qt[t].2;
 
-            let location = if t1_len + b1_len == self.params.buffer_size_q {
-                if t1_len < self.params.buffer_size_q {
-                    let (del, _, _) = self.arc_b1.pop_lru().unwrap();
-                    self.arc_dir[del.tenant.index()].remove(&del.page);
-                    self.replace(t, List::T1) // HACK: using T1 as !B2 (FIXME)
-                } else {
-                    debug_assert!(t1_len == self.params.buffer_size_q);
-                    let (del, _, location) = self.pop_lru(List::T1, t).unwrap();
-                    self.arc_dir[del.tenant.index()].remove(&del.page);
-                    self.counters[del.tenant.index()].evictions += 1;
-                    location
-                }
-            } else if t1_len + t2_len + b1_len + b2_len >= self.params.buffer_size_q || at_qmax {
-                debug_assert!(t1_len + b1_len < self.params.buffer_size_q);
-                if t1_len + t2_len + b1_len + b2_len == 2 * self.params.buffer_size_q {
-                    let (del, _, _) = self.arc_b2.pop_lru().unwrap();
-                    self.arc_dir[del.tenant.index()].remove(&del.page);
-                }
+            let location = if self.len() >= self.params.buffer_size_q || at_qmax {
                 self.replace(t, List::T1) // HACK: using T1 as !B2 (FIXME)
             } else {
                 self.max_loc += 1;
                 self.max_loc
             };
+
+            if self.arc_b2.len() > self.params.buffer_size_q - self.arc_p {
+                let (del, _, _) = self.arc_b2.pop_lru().unwrap();
+                self.arc_dir[del.tenant.index()].remove(&del.page);
+            }
+
+            if self.arc_b1.len() > self.params.buffer_size_q {
+                let (del, _, _) = self.arc_b1.pop_lru().unwrap();
+                self.arc_dir[del.tenant.index()].remove(&del.page);
+            }
 
             self.push_mru_update_dir(List::T1, (op, self.op_time, location));
 

commit b5a9495d61ccce427af8ce4c8620db6b304f73cc

Fix trimming of b1 and b2 lists in all miss case (IV)
Fixes: ba5ab6f49813

src/main.rs
@@ -200,13 +200,13 @@ impl Buffer {
                 self.max_loc
             };
 
-            if self.arc_b2.len() > self.params.buffer_size_q - self.arc_p {
-                let (del, _, _) = self.arc_b2.pop_lru().unwrap();
+            if self.arc_b1.len() > self.params.buffer_size_q - self.arc_p {
+                let (del, _, _) = self.arc_b1.pop_lru().unwrap();
                 self.arc_dir[del.tenant.index()].remove(&del.page);
             }
 
-            if self.arc_b1.len() > self.params.buffer_size_q {
-                let (del, _, _) = self.arc_b1.pop_lru().unwrap();
+            if self.arc_b2.len() > self.arc_p {
+                let (del, _, _) = self.arc_b2.pop_lru().unwrap();
                 self.arc_dir[del.tenant.index()].remove(&del.page);
             }
 