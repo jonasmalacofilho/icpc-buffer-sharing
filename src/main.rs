4eb9c9757ffbdfd05b220b7d5f73e74f9978eecc
Jonas Malaco <jonas@protocubo.io>
2023-04-26 23:11:21 -0300

    Revert "Sanity check"
    This reverts commit b931adeaab217aab40c998c0783f41dfa09db8b0.

--- src/main.rs
+++ src/main.rs
@@ -179,7 +179,7 @@ impl Buffer {
                 if map.len() > *qbase {
                     (0, 0, used)
                 } else {
-                    (1, 0, used)
+                    (1, self.counters[*donor].preventable_misses, used)
                 }
             })
             .unwrap();
